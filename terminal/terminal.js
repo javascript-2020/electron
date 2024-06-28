badfba74780183f427888727da39fe3ef8f5ba29
matt <javascript.12.03.2024@gmail.com>
2024-06-28 21:11:22 +0100

    update terminal.js

--- terminal/terminal.js
+++ terminal/terminal.js
@@ -16,12 +16,10 @@
         var {app,BrowserWindow}  = require('electron');
         var webPreferences       = {nodeIntegration:true,contextIsolation:false};
         app.whenReady().then(()=>{
-              var win   = new BrowserWindow({webPreferences});
+              var win   = new BrowserWindow({webPreferences,icon});
               win.maximize();
               win.webContents.openDevTools();
               win.loadURL(`data:text/html;base64,${btoa(html)}`);
-              
-              
         });
         
         
@@ -65,5 +63,20 @@
               </script>
         `;
 
+        var icon    =
+        'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAAAAXNSR0IArs4c6QAAA0lJREFUeF7tm0'+
+        '1oE0EUx/+7W3oprVuJRjyYfiSQg0crnloIeCqK4DUVvHlU8eKhUuxBEFGEHrxqc/Ai+EFPYqA5ifamh0LTrxzEarBrSxFKZlfGOH'+
+        'R3s9tqNrO73Zk5hZCdzPvNm/+b93ZGsSxLgcBNkQCkB8glIDVAYA2EFEG/KJDL5Xrr9frRJHhHKpX6Ua1Wt7xsbfEAavjq6upF0z'+
+        'QnLMvKJwGAoiiLqqrODg4OvnaDcACgxq+srDwyTfMKgK4kGG+zoaGq6rOhoaEbS0tL2+x7BwBN0+6YpjmVMMMd5qiqOkUIudsCoL'+
+        '+/P2MYxjyAU0kGAKCm6/qYYRhrVBP+eICiKJamaeOEkJcJdH33fDY0TbtECJlzAygSQmbtv+7qSSFdmERXzzFYMA+VYyhQ0dj5jo'+
+        '3yNBo7dcfYNU2bIISUDgTQrWcwfK2Cbv1wropdo4blJ6PYNdYlADsB6QFyCUgNkCIYOAqYFqD+rSTYP8cpVnKNAmczwOhw09zKMv'+
+        'DBGWliwYELADrb5waA6XEg3du0c2MbmKkAbz4DcfIGLgCowbcKQHGkdZJLH4EH5fhACB0ARUIhlBaALz/3NCKq9cAFgNcScBs4Xw'+
+        'XuvY0eAhcAzFgqgnYdcENY/AY8LAPv16LzBK4AmCfcLAD5495OTsVxci66CMEVADWZQjh5BLh9HhjL+kOIKkJwB2CHUDzjHRkYFh'+
+        'YhwhTEUAAwCHRX6Bceo4IQGgA7hAunm+Lo1agwXn8RXnSQAMKuCAm5BFgkEFYET/QJGgaF3wgJuxXuVDJE++lUY5Upd3/cwuB+av'+
+        '+v6TDVjk61r1vePYUO4KCCiD13yKc7Y/7mL2BhvVmICcUDgpbEHl/2T5yCIKFZ56tPztSbmwfQgf5vUZTN/tPiXi0xiMHuZ72SLa'+
+        '4A2i2LP7/qXz8IAoQCuP8uRA9od7AHhc92+vUrwXH1gHYGyp6hEWCkQ2/cadWptumdYcYWAEufg0B0P+u1FxACgN8miAKKNYBOzr'+
+        '5fXxJA0IKIaIekxD4mp+v6gLAHJZmICH1UlkIQ/rA0PTKbzWb7hD0uz5YCAyHkhQn7poKCCGPDwvs/9rsbKe8MyZuj8uaovDkqb4'+
+        '7yVuE49/8bpiNLDDjNf90AAAAASUVORK5CYII='
+        ;
         
           
