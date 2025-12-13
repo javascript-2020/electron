0188b32d7e717f787a55ea5e1b5bad17c606c880
matt <javascript.12.03.2024@gmail.com>
2025-12-13 21:03:28 +0000

    save file

---
+++ editor/file-io.js
@@ -0,0 +1,38 @@
+
+
+
+// file-io.js
+
+
+
+        var {ipcMain}   = require('electron');
+        var fs          = require('fs');
+        var fsp         = fs.promises;
+
+
+
+        var file    = {};
+
+
+        
+        ipcMain.handle('read-file',async function(event,{path}){return file.read.apply(null,[...arguments].slice(1))});
+        ipcMain.handle('write-file',async function(event,{path,buf}){return file.write.apply(null,[...arguments].slice(1))});
+
+        
+        
+        file.read   = function({path}){
+          
+              return fs.readFileSync(path);
+              
+        }//read
+        
+        
+        file.write    = function({path,buf}){
+          
+              return fs.writeFileSync(path,buf);
+              
+        }//write
+        
+        
+
+
