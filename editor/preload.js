41bb2a1b41d622c0fbb0d6430b983618bbcd3184
matt <javascript.12.03.2024@gmail.com>
2025-12-13 21:29:11 +0000

    save file

--- editor/preload.js
+++ editor/preload.js
@@ -6,10 +6,10 @@
         
         contextBridge.exposeInMainWorld('electronAPI',{
           
-              readFile: ({path})=>ipcRenderer.invoke('read-file',{path}),
-              
+              readFile      : ({path})=>ipcRenderer.invoke('read-file',{path}),
+              writeFile     : ({path,buf})=>ipcRenderer.invoke('write-file',{path,buf}),
 
-              onOpenFile:callback=>{
+              onOpenFile    : callback=>{
                                                                                 console.log('onOpenFile');
                   ipcRenderer.on('open-file',(event,{path})=>{
                                                                                 console.log('open-file');
